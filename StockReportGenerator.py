e54237e097d999f79e4b0534e55e7875603d3a92
AlvinVJ <alvinvinodjohn123@gmail.com>
2024-07-16 21:09:33 +0530

    now saves password to a external file and initializes password value from it

---
+++ StockReportGenerator.py
@@ -0,0 +1,26 @@
+import json
+import os
+
+password = ""
+
+def initializeVars():
+    global password
+    with open("./settings.json", 'r') as f:
+        data = json.load(f)
+        password = data['password']
+
+def initializeJSON():
+    data = {
+        "mysqlPassword" : "" 
+    }
+    data['password'] = input("\nEnter mysql password:\t")
+    with open('./settings.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+
+if not os.path.exists('./settings.json'):
+    initializeJSON()
+else:
+    initializeVars()
+
+print(password)
